da755eb2bb74dbfdb00d7b1422cc679fe4fc4347
thistle <neoffrost@gmail.com>
2015-07-03 10:31:22 +0900

    URL includes를 이용한 리팩토링

--- superlists/urls.py
+++ superlists/urls.py
@@ -14,12 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
 from django.conf.urls import include, url
-from django.contrib import admin
+# from django.contrib import admin
 
 urlpatterns = [
-	url(r'^$', 'lists.views.home_page', name='home'),
-	url(r'^lists/(\d+)/$', 'lists.views.view_list', name='view_list'),
-         url(r'^lists/(\d+)/add_item$', 'lists.views.add_item', name='add_item'),
-	url(r'^lists/new$', 'lists.views.new_list', name='new_list'),
+    url(r'^$', 'lists.views.home_page', name='home'),
+    url(r'^lists/', include('lists.urls')),
     # url(r'^admin/', include(admin.site.urls)),
 ]
